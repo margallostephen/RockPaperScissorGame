74ca62702873ec68f84f09ad9e9e13dac61653b4
margallostephen <spmargallo@gmail.com>
2022-11-20 01:27:48 +0800

    Completed the game UI

--- RockPaperScissorGame/GameForm.cs
+++ RockPaperScissorGame/GameForm.cs
@@ -11,5 +11,30 @@ namespace RockPaperScissorGame
         {
 
         }
+
+        private void RockBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PaperBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ScissorBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PickBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PlayAgainBtn_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
